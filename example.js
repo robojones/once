df1b62bc44de68a0fc5d88e2668b75127a7466a9
robojones <contact@jonathan-scholz.de>
2017-11-24 20:15:17 +0100

    add example

---
+++ example.js
@@ -0,0 +1,17 @@
+const once = require('once').default
+const { EventEmitter } = require('events')
+
+const emitter = new EventEmitter()
+
+
+setTimeout(() => {
+  emitter.emit('continue')
+}, 5000)
+
+console.log('start')
+
+// create and await a promise for the "continue" event.
+// It should take 5s until the promise gets resolved.
+await once(emitter, 'continue')
+
+console.log('end :D')
